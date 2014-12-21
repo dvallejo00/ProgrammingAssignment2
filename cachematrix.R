b64a78e1eba680d976290e1be9daa80940605444
David Vallejo <dvallejo00@gmail.com>
2014-12-20 21:57:23 -0600

    Final Submit
    This submit, clean some coments and show a script more understandable.

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,12 @@
-## This script solve the inverse Matrix, makeCacheMatrix acts as a Object in Object Oriented Programming
-## CacheSolve will check, if data has been calcudated will get info from cache, if not, will calculate it. 
+## This Script calculate the Inverse Matrix, with the help of two functions: makeMatrix and cacheSolve
+## In case that data exist,  data will be retrieved from Cache, if not,  it will be calculated
 
 
-## Set & Get the function
+## makeCacheMatrix acts as a auxiliar, with setting getting capabilities similar to Object Oriented Programming
+## This function isnt used directly, it is used for create an Object that will be used for cacheSolve Function.
+## It holds a List with the 4 funtctions set/get thar work with the  Matrix Received and the Data that will be cached.
+
+
 
 makeCacheMatrix <- function(x = matrix()) {
   
@@ -25,15 +29,19 @@ list(set=set, get=get,setmatrix=setmatrix,getmatrix=getmatrix)
 
 
 
-## If m is not null, will retrieve the cache data, if not, will calculate it.
+## cacheSolve is a function that receive a makeMatrix Object as argument and try to get the Inverse Matrix if it exist
+## If Inverse matrix exist in Cache, it is get from cache and showed with a message 
+## If Inverse Matrix doesnt exist, it is calculated and setted in the object in cache for the next time when it could be retrieved
 
 cacheSolve <- function(x=matrix(), ...) {
-        ## Return a matrix that is the inverse of 'x'
+
 m<-x$getmatrix()
 if(!is.null(m)){
+  
   message("Data has been get from Cache")
   return(m)
 }
+
 matrix<-x$get()
 m<-solve(matrix,...)
 x$setmatrix(m)
