cd29e6d276a64db44b1fd1e5cc767ce7f84c8689
David Vallejo <dvallejo00@gmail.com>
2014-12-20 18:17:13 -0600

    myFirstSubmition
    First submition of 2 assigment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This script solve the inverse Matrix, makeCacheMatrix acts as a Object in Object Oriented Programming
+## CacheSolve will check, if data has been calcudated will get info from cache, if not, will calculate it. 
 
-## Write a short comment describing this function
+
+## Set & Get the function
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+m<-NULL
+
+  set<-function(y){
+    x<<-y
+    m<-NULL
+  }
+
+get<-function()x
+
+
+setmatrix<-function(solve)
+  m<<-solve
+getmatrix<-function()m
+
+list(set=set, get=get,setmatrix=setmatrix,getmatrix=getmatrix)
 
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+## If m is not null, will retrieve the cache data, if not, will calculate it.
+
+cacheSolve <- function(x=matrix(), ...) {
         ## Return a matrix that is the inverse of 'x'
+m<-x$getmatrix()
+if(!is.null(m)){
+  message("Data has been get from Cache")
+  return(m)
+}
+matrix<-x$get()
+m<-solve(matrix,...)
+x$setmatrix(m)
+m
 }
